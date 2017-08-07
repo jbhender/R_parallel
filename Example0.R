1ebca7e24a7cf5d22bbeb6ec772a40ac313f5ba8
jbhender <jbhender@umich.edu>
2017-08-07 17:15:02 -0400

    Minor changes.

--- Example0.R
+++ Example0.R
@@ -10,48 +10,56 @@ foo = load('./YaleTNBC.Rdata')
 AA = grep('AA',colnames(YaleTNBC))
 EA = grep('EA',colnames(YaleTNBC))
 
-### Version 1 ###
+### Version 1 - for loop ###
 t1 = system.time({
  fold_change1 = rep(NA,nrow(YaleTNBC))
  for(i in 1:nrow(YaleTNBC)){
-   fold_chang1[i] = mean(YaleTNBC[i,AA]) - mean(YaleTNBC[i,EA])
+   fold_change1[i] = mean(YaleTNBC[i,AA]) - mean(YaleTNBC[i,EA])
  }
 })
 
+### Version 2 - an apply function ###
 t2 = system.time({
-  fold_change = sapply(1:nrow(YaleTNBC),function(i){
+  fold_change2 = sapply(1:nrow(YaleTNBC),function(i){
     mean(YaleTNBC[i,AA]) - mean(YaleTNBC[i,EA])
   })
 })
 
+fc2 = function(i) mean(YaleTNBC[i,AA]) - mean(YaleTNBC[i,EA])
+t2a = system.time({
+ fold_change2a = sapply(1:nrow(YaleTNBC),fc2)
+})
+
+## version 3 - a different apply approach ##
 t3 = system.time({
-  fold_change = apply(YaleTNBC[,AA],1,mean) - apply(YaleTNBC[,EA],1,mean)
+  fold_change3 = apply(YaleTNBC[,AA],1,mean) - apply(YaleTNBC[,EA],1,mean)
 })
 
 
-## Version 2 ##
+## Version 4 - parallel approaches ##
 library(parallel)
 t4 = system.time({
-  fold_change = mclapply(1:nrow(YaleTNBC),function(i){
+  fold_change4 = mclapply(1:nrow(YaleTNBC),function(i){
   mean(YaleTNBC[i,AA]) - mean(YaleTNBC[i,EA])
 })
 })
 class(fold_change4)
-fold_change = unlist(fold_change)
+fold_change4 = unlist(fold_change4)
 
 
 # Check and increase number of cores used #
 getOption("mc.cores", 2L)
 detectCores()
-help(mc.preschedule)
+#help(mc.preschedule)
 
+## Version 5 - More cores ##
 t5 = system.time({
   fold_change = mclapply(1:nrow(YaleTNBC),function(i){
     mean(YaleTNBC[i,AA]) - mean(YaleTNBC[i,EA])
   },mc.cores=4)
 })
 
-rbind(t1,t2,t3,t4,t5)
+print(rbind(t1,t2,t3,t4,t5))
 
 ## An example of parallelism gone wrong ##
 t6 = system.time({
